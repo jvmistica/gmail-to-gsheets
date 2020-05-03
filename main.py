cb531e5cabc0a4d38b157a6002c6f2b9ac3c1587
Joann Mistica <zombossjoey@tutanota.com>
2020-05-03 19:45:48 +0800

    Added sample to extract CSV contents from email and append them to Google Sheets

--- main.py
+++ main.py
@@ -29,9 +29,11 @@ for message in messages:
             with open(path, "wb") as f:
                 f.write(file_data)
 
+
 ################################################################
 # Read, update, and append values to Google Sheets.            #
 ################################################################
+
 spreadsheet = "<spreadsheet_id>"
 service = gsheets.create_service()
 
@@ -47,3 +49,27 @@ gsheets.update_spreadsheet(service, spreadsheet, "<tab_name>!A8", values)
 # Append to spreadsheet
 values = {"values": [["This goes at the end.", "This one too."]]}
 gsheets.append_spreadsheet(service, spreadsheet, "<tab_name>", values)
+
+
+################################################################
+# Extact CSV contents from the email attachment                #
+# and append them to Google Sheets.                            #
+################################################################
+
+for message in messages:
+    msg = gmail.read_message(gmail_service, user_id, message.get("id"))
+    subject = next(header["value"] for header in msg["payload"]["headers"] if header["name"] == "Subject")
+    parts = msg["payload"]["parts"]
+    message_id = msg["id"]
+    for part in parts:
+        attachment_id = part["body"].get("attachmentId")
+        if attachment_id:
+            attachment = gmail_service.users().messages().attachments().get(userId=user_id,
+                                                                      messageId=message_id,
+                                                                      id=attachment_id
+                                                                      ).execute()
+            data = attachment["data"]
+            file_data = base64.urlsafe_b64decode(data.encode("UTF-8"))
+            file_data = [data.split(",") for data in file_data.decode().split("\r\n")]
+            values = {"values": file_data}
+            gsheets.append_spreadsheet(service, spreadsheet, "<tab_name>", values)
